da3296011889cf463e74aa168fc49ef657f6719a
Jonas Claeson <jonas.cl@protonmail.com>
2019-02-28 13:06:27 +0100

    Add an empty line between each group

--- sesam/run.sh
+++ sesam/run.sh
@@ -6,5 +6,6 @@ file=sesam-2019.txt
 cp $file tmp
 
 . dist.sh tmp |
-	awk '{print "grep \" " $2 " \" tmp ; grep -v \" " $2 " \" tmp > tmp2 ; mv tmp2 tmp"}' |
-	sh
+	awk '{print "grep \" " $2 " \" tmp ; grep -v \" " $2 " \" tmp > tmp2 ; mv tmp2 tmp ; echo \"\""}' |
+	sh |
+	uniq
